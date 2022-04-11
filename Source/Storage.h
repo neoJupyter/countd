4c943a317d8741c694e4362e0b866e4e2c7936cb
DWVoid <yshliu0321@icloud.com>
2022-04-12 03:51:12 +0800

    Implement Remote Storage Client and Storage Abstraction

--- Source/Storage.h
+++ Source/Storage.h
@@ -2,16 +2,18 @@
 
 #include <utility>
 #include <string_view>
+#include <kls/io/IP.h>
 #include <kls/temp/STL.h>
-#include <kls/coroutine/ValueAsync.h>
+#include <kls/coroutine/Async.h>
 
 namespace count {
     using namespace kls;
-    using namespace kls::coroutine;
-    using namespace kls::essential;
 
     struct IStorage {
-        virtual ValueAsync<temp::vector<int64_t>> fetch(temp::vector<std::string_view> names) = 0;
-        virtual ValueAsync<void> store(temp::vector<std::pair<std::string_view, int64_t>> elements) = 0;
+        virtual coroutine::ValueAsync<temp::vector<int64_t>> fetch(temp::vector<std::string_view> names) = 0;
+        virtual coroutine::ValueAsync<> store(temp::unordered_map<std::string_view, int64_t> elements) = 0;
+        virtual coroutine::ValueAsync<> close() = 0;
     };
+
+    coroutine::ValueAsync<std::shared_ptr<IStorage>> connect_remote_storage(io::Peer remote);
 }
